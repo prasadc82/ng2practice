1b10e5aa8bcb6d518f1ce722345902935b2a29df
lokeshjain2008 <lokeshjainajay@yahoo.com>
2016-05-07 10:10:40 +0530

    used datalist to create typehead with lot of problems

--- src/components/repo-finder.ts
+++ src/components/repo-finder.ts
@@ -3,10 +3,15 @@ import {RepoList} from '../services/repo-list';
 import {Control} from 'angular2/common';
 
 import {Observable} from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
+
+//alternatively all the imports can be written as
+import 'rxjs/Rx';
 
 @Component({
   selector: 'repo-finder',
@@ -14,20 +19,51 @@ import 'rxjs/add/operator/switchMap';
   styleUrls:['src/components/repo-finder.css'],
   providers: [RepoList]
 })
+// export class RepoFinder {
+//   @Input() placeholder : string;
+//   @Input() datalistId: string;
+//   @Output() stats = new EventEmitter();
+
+//   repos: Observable<Array<string>>;
+//   repoNameStartsWith = new Control();
+//   repoName = new Control();
+
+//   constructor(private _repoList: RepoList){
+//     this.repos = this.repoNameStartsWith.valueChanges
+//                           .debounceTime(400)
+//                           .distinctUntilChanged()
+//                           .switchMap((repoNameStartsWith:string) => {
+//                             return this._repoList.getRepos(repoNameStartsWith);
+//                           });
+//   };
+// }
+
+// alternate way of doing
 export class RepoFinder {
   @Input() placeholder : string;
+  @Input() datalistId: string;
   @Output() stats = new EventEmitter();
-  
-  repos: Observable<Array<string>>;
-  repoNameStartsWith = new Control();
+
+  repoNameStartsWith :string = '';
+
+
+  private searchTermStream = new Subject<string>();
   repoName = new Control();
-  
+
   constructor(private _repoList: RepoList){
-    this.repos = this.repoNameStartsWith.valueChanges
-                          .debounceTime(400)
-                          .distinctUntilChanged()
-                          .switchMap((repoNameStartsWith:string) => {
-                            return this._repoList.getRepos(repoNameStartsWith);
-                          });
+
   };
-};
+
+  searchRepo(value: string){
+    if(!value) {
+      return;
+    }
+    this.searchTermStream.next(value);
+  }
+
+  repos:Observable<string[]> = this.searchTermStream
+    .debounceTime(900)
+    .distinctUntilChanged()
+    .switchMap((term:string) => this._repoList.getRepos(term));
+
+}
